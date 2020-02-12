ec04120ecc6dced77bca2b76ae95588c49652162
Alice104 <415083240@qq.com>
2020-02-12 15:30:49 +0900

    test git

---
+++ hello.py
@@ -0,0 +1,26 @@
+import socket
+
+print("hello\n")
+print("world")
+
+
+s1=set('abcdde')
+s2=set([1,2,3,4,5])
+s3 = frozenset("xuanhun")
+
+for item in s2:
+    print(item)
+
+dict ={'Name':'Zara','Age':7,'Class':'First'}
+del dict['Age']
+print(dict)
+
+
+dict.clear()
+
+print(dict)
+
+del dict
+
+
+print(dict)
